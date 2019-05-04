9eec63eeebe857f187e20f0dd75b08991f072570
Rob Simpson <rsimpson@ventera.com>
2019-05-04 00:45:52 -0400

    Remove meals

--- src/app/health/shared/components/list-item/list-item.component.ts
+++ src/app/health/shared/components/list-item/list-item.component.ts
@@ -1,15 +1,67 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
 
 @Component({
-	selector: 'list-item',
+  selector: 'list-item',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 	styleUrls: ['list-item.component.scss'],
   template: `
     <div class="list-item">
-      {{ item | json }}
+      <a [routerLink]="getRoute(item)">
+
+        <p class="list-item__name">{{ item.name }}</p>
+        <p class="list-item__ingredients">
+          <span>
+            {{ item.ingredients }}
+          </span>
+        </p>
+
+      </a>
+
+      <div
+        class="list-item__delete"
+        *ngIf="toggled">
+        <p>Delete item?</p>
+        <button
+          class="confirm"
+          type="button"
+          (click)="removeItem()">
+          Yes
+        </button>
+        <button
+          class="cancel"
+          type="button"
+          (click)="toggle()">
+          No
+        </button>
+      </div>
+
+      <button
+        class="trash"
+        type="button"
+        (click)="toggle()">
+        <img src="/img/remove.svg">
+      </button>
     </div>
   `
 })
 export class ListItemComponent {
 	@Input()
-	item: any;
+  item: any;
+
+  @Output()
+  remove = new EventEmitter<any>();
+
+  toggled = false;
+
+  toggle() {
+    this.toggled = !this.toggled;
+  }
+
+  removeItem() {
+    this.remove.emit(this.item);
+  }
+
+  getRoute(item: any) {
+    return [`../meals`, item.$key];
+  }
 }
